377729695d3059767eef40f4a567308c88220210
Lenochka1972 <lenadubinina667@gmail.com>
2025-12-03 17:52:33 +0300

    Add files via upload

--- Lesson_7.py
+++ Lesson_7.py
@@ -1,31 +1,26 @@
 from pytimeparse import parse
 
-import os
 import ptbot
 
-from dotenv import load_dotenv
+from decouple import config
 
-LOAD_DOYEBV = load_dotenv()
-TG_TOKEN = os.getenv('CookEggs_TOKEN')
-TG_CHAT_ID = os.getenv('TG_ID')
+TG_TOKEN = config('CookEggs_TOKEN')
+TG_CHAT_ID = config('TG_ID')
 
 
 def notify_progress(
                     secs_left,
                     auther_id,
                     id_message_notify,
-                    id_progressbar_notify,
                     max_secs_notify,
                     bot
                     ):
-    message_notify = 'Осталось секунд: {}'.format(secs_left)
-    bot.update_message(auther_id, id_message_notify, message_notify)
-    bot.update_message(
-                       auther_id,
-                       id_progressbar_notify,
-                       render_progressbar(max_secs_notify,
+    message_notify_1 = 'Осталось секунд: {}'.format(secs_left)
+    message_notify_2 = render_progressbar(max_secs_notify,
                                           max_secs_notify-secs_left)
-                       )
+    message_notify = message_notify_1 + message_notify_2
+
+    bot.update_message(auther_id, id_message_notify, message_notify)
 
 
 def render_progressbar(
@@ -42,19 +37,17 @@ def render_progressbar(
     percent = percent.format(100 * (iteration / float(total)))
     filled_length = int(length * iteration // total)
     pbar = fill * filled_length + zfill * (length - filled_length)
-    return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix)
+    return '\n{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix)
 
 
 def wait(chat_id, pause_sec, bot):
     secs_left = max_secs = parse(pause_sec)
     id_message = bot.send_message(chat_id, 'Запускаю таймер')
-    id_progressbar = bot.send_message(chat_id, render_progressbar(max_secs, 0))
     bot.create_countdown(
                          secs_left,
                          notify_progress,
                          auther_id=chat_id,
                          id_message_notify=id_message,
-                         id_progressbar_notify=id_progressbar,
                          max_secs_notify=max_secs,
                          bot=bot
                          )
